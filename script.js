86ad1cffb4300ad1ec3f007817187540e51013f1
SMK <selvamuthukumar038@gmail.com>
2024-01-30 11:37:05 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
+document.getElementById("crpass").addEventListener("input",function(){
+  store();
+})
 
+function store(){
+  var val = document.getElementById("crpass").value;
+  localStorage.setItem("cnfrmpass",val);
+}
+function checkpass(){
+  var pass1 = document.getElementById("crpass").value;
+  var pass2 = document.getElementById("copass").value;
+  if(pass1==="" || pass2===""){
+    alert("Please Enter Password")
+  }
+  else if(pass1===pass2 ){
+    document.getElementById("signinform").submit();
+  }
+  else{
+    alert("Password Mismatch");
+  }
+}
+function checkpass2(){
+  var logpass1 = document.getElementById("logpass").value;
+  var logpass2 = localStorage.getItem("cnfrmpass");
+  if(logpass1===logpass2){
+      document.getElementById("loginform").submit();
+  }
+  else{
+    alert("Invalid email or Password");
+  }
+}
 
 let map;
 
